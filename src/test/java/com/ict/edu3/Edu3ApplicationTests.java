3d937e8deb695ceb290b4dd756f87e23f4b4afb6
parkchaelim <pcl8205@naver.com>
2023-09-05 12:43:41 +0900

    0905

---
+++ src/test/java/com/ict/edu3/Edu3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.edu3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edu3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
